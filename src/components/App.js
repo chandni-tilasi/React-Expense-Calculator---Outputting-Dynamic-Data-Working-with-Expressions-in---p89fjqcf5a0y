593854190a080bc3ac1edf7c93ca01015bcf2f4a
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-01-25 19:14:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from 'react'
+ import '../styles/App.css';
 
 const App = () => {
 
+  const [expenses , setexpences] = useState(0);
+  const [input,setInput] = useState("");
+  const [arr,setArr] = useState([]);
+  const [paisa,setPaisa] = useState(0)
+  function handleChange(e){
+    setInput(e.target.value);
+   
+  }
+
+  function handleClick(){
+    
+    const str = input.indexOf("-");
+    setArr([...arr,input])
+    const price = +input.slice(str+1,input.length);
+    setPaisa(paisa+price);
+    console.log(paisa)
+    
+  }
+
+  
+
   return (
     <div id="main">
-      <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <input id="expense-input" value={input} onChange={handleChange} />
+      <button id="expense-button" onClick={handleClick} >Add Expense</button>
       <div id="expense-list">
+        <ul>
+          {
+            arr.map((item,index)=>{
+              return <li key={`item${index+1}`} > {item} </li>
+            })
+          }
+        </ul>
       </div>
       <div id="total-expense">
-        Total Expense: 0
+        Total Expense: {`${paisa}`}
       </div>
     </div>
   )
